2ba94d2a10330699184fdda4ea1f5b03bf57eab7
modi2207 <modichirag2207@gmail.com>
2025-10-10 00:05:57 +0530

    9th

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,6 @@ config = rx.Config(
     app_name="app",
     plugins=[rx.plugins.TailwindV3Plugin()],
     frontend_port=3000,
-    backend_port=8000
+    backend_port=8000,
+    deploy_url="https://modi2207.github.io/knt_associates_web_app/",
 )
